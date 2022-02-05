c606832d9e6e26e0b3df0822d9842abce11b16ef
saffet <saffetk502@gmail.com>
2022-02-05 12:47:53 +0100

    Dritte commit 3. mal gemacht

--- src/git_project/GitErsteClass.java
+++ src/git_project/GitErsteClass.java
@@ -4,9 +4,9 @@ public class GitErsteClass {
 
 	public static void main(String[] args) {
 		
-		System.out.println("erste commit wurde erstellt von local repository");
-		System.out.println("zweite commit wurde erstellt von local repository");
-
+		System.out.println("erste commit wurde von local repository erstellt");
+		System.out.println("zweite commit wurde von local repository erstellt");
+		System.out.println("dritte commit wurde von local repository erstellt");
 	}
 
 }
