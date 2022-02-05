8dce324a70e73f6446843a80a75e5b9970407c79
saffet <saffetk502@gmail.com>
2022-02-05 13:14:16 +0100

    Ende Konflikt

--- src/git_project/GitErsteClass.java
+++ src/git_project/GitErsteClass.java
@@ -8,7 +8,7 @@ public class GitErsteClass {
 		System.out.println("zweite commit wurde von local repository erstellt");
 
 		
-		System.out.println("Konflikt wurde erstellt");
+		System.out.println("2. mal Konflikt wurde erstellt");
 
 		System.out.println("erste commit wurde von remote repository erstellt");
 	}
